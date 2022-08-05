6383a5fa771493cd9ede2e1d256631ae229f2192
phucvhd <75359238+phucvhd@users.noreply.github.com>
2022-08-05 17:34:30 +0700

    add setup

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,35 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expense = [
+    { id: "e1", title: "Car", amount: "94.12", date: new Date(2020, 7, 4) },
+    { id: "e2", title: "TV", amount: "294.12", date: new Date(2020, 3, 2) },
+    { id: "e3", title: "Apple", amount: "394.12", date: new Date(2020, 5, 1) },
+    { id: "e4", title: "Phone", amount: "494.12", date: new Date(2020, 6, 2) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
